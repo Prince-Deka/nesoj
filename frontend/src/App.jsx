2d163d715c468d404fb6455bc6f2bc686aa8ad3e
Prince-Deka <princedeka00@gmail.com>
2024-03-29 04:27:22 +0530

    backend variables added for deployement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} /> {/* 👈 Renders at /app/ */}
         <Route path="/register" element={<Signup/>}/>
-        <Route path='/login' element={<Login />} />
+        <Route path="/login" element={<Login />} />
           
       </Routes>
 
