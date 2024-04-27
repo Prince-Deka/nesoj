530ae29bb1d2cd865d24cf2ac6280073988b2a1c
Mrigankar Sonowal <mrigankarofficial@gmail.com>
2024-04-27 14:55:25 +0530

    Updated Gallery

--- frontend/src/components/Landing/NavBar/Navbar.jsx
+++ frontend/src/components/Landing/NavBar/Navbar.jsx
@@ -59,6 +59,16 @@ function Navbar() {
               <li className="nav-item">
                 <Link to="/gallery" className={`nav-link ${isActive('/gallery') ? 'active' : ''}`}>Gallery</Link>
               </li>
+              <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Gallery
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Photos</a></li>
+            <li><a class="dropdown-item" href="#">Videos</a></li>
+            <li><a class="dropdown-item" href="#">All</a></li>
+          </ul>
+        </li>
               <li className="nav-item">
                 <Link to="/posts" className={`nav-link ${isActive('/posts') ? 'active' : ''}`}>Posts</Link>
               </li>
