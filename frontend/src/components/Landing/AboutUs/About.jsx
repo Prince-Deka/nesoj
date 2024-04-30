5f2db0f9e632713b36b1d6b00f0331755ededbf8
Mrigankar Sonowal <mrigankarofficial@gmail.com>
2024-04-30 18:06:08 +0530

    Forgot the changes but it was major

--- frontend/src/components/Landing/AboutUs/About.jsx
+++ frontend/src/components/Landing/AboutUs/About.jsx
@@ -73,13 +73,16 @@ export default function About() {
   };
 
   const items = [
-    { id: 2, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' },
-    { id: 1, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' },
-    { id: 3, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' },
-    { id: 4, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' },
-    { id: 5, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' },
-    { id: 6, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' },
+    { id: 1, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' , name: 'Arunachal Students\' Club Jalandhar' },
+    { id: 2, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' , name: 'Assam Students\' Union Jalandhar' },
+    { id: 3, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' , name: 'Jalandhar Mizo Zirlai Pawl' },
+    { id: 4, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' , name: 'Manipur Students\' Union Phagwara' },
+    { id: 5, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' , name: 'Meghalaya Students\' Union Jalandhar' },
+    { id: 6, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' , name: 'Naga Students\' Union Phagwara' },
+    { id: 7, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' , name: 'Sikkimese Gorkha Students\' Union Jalandhar' },
+    { id: 8, src: 'assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg' , name: 'Tripura Students\' Association Jalandhar' }
   ];
+
 
   return (
     <div>
@@ -97,7 +100,7 @@ export default function About() {
             {items.map(item => (
               <div key={item.id} className="px-3">
                 <div className="card card-body cards-aboutUs-Landing">
-                  <Card/>
+                  <Card name={item.name}/>
                 </div>
               </div>
             ))}
@@ -113,7 +116,9 @@ export default function About() {
             {items.map(item => (
               <div key={item.id} className="px-3">
                 <div className="card card-body cards-aboutUs-Landing">
-                  <img className="img-fluid" src={item.src} alt={`Slide ${item.id}`} />
+                  <img className="img-fluid StateLogo" src={item.src} alt={`Slide ${item.id}`} />
+                  <div className="orgNameDiv"><p className="card-title OrgName">{item.name}</p></div>
+                  <button class="custom-btn orgDetailsBtn"><span>Read More</span></button>
                 </div>
               </div>
             ))}
--- frontend/src/components/Landing/State/State.jsx
+++ frontend/src/components/Landing/State/State.jsx
@@ -1,13 +1,173 @@
-import React from 'react'
-import Navbar from '../NavBar/Navbar'
+import React from "react";
+import Slider from "react-slick";
+import Navbar from "../NavBar/Navbar";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../AboutUs/About.css";
+import Footer from "../Footer/Footer";
+import "./State.css";
+import Card from "../../CardTemp/Card";
 
-function State() {
+function NextArrow(props) {
+  const { className, style, onClick } = props;
   return (
-    <div>
-        <Navbar/>
-        State
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "#fff",
+        borderRadius: "50%",
+      }}
+      onClick={onClick}
+    >
+      <i
+        className="fa fa-chevron-right"
+        style={{ color: "#0ac5ab", fontSize: "30px" }}
+      ></i>
     </div>
-  )
+  );
 }
 
-export default State
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "#fff",
+        borderRadius: "50%",
+      }}
+      onClick={onClick}
+    >
+      <i
+        className="fa fa-chevron-left"
+        style={{ color: "#0ac5ab", fontSize: "30px" }}
+      ></i>
+    </div>
+  );
+}
+
+function State() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const items = [
+    {
+      id: 1,
+      src: "assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg",
+      name: "Arunachal Students' Club Jalandhar",
+    },
+    {
+      id: 2,
+      src: "assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg",
+      name: "Assam Students' Union Jalandhar",
+    },
+    {
+      id: 3,
+      src: "assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg",
+      name: "Jalandhar Mizo Zirlai Pawl",
+    },
+    {
+      id: 4,
+      src: "assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg",
+      name: "Manipur Students' Union Phagwara",
+    },
+    {
+      id: 5,
+      src: "assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg",
+      name: "Meghalaya Students' Union Jalandhar",
+    },
+    {
+      id: 6,
+      src: "assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg",
+      name: "Naga Students' Union Phagwara",
+    },
+    {
+      id: 7,
+      src: "assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg",
+      name: "Sikkimese Gorkha Students' Union Jalandhar",
+    },
+    {
+      id: 8,
+      src: "assets/StateLogos/500x500/ASUJ/ASUJ_500x500.jpg",
+      name: "Tripura Students' Association Jalandhar",
+    },
+  ];
+
+  return (
+    <div>
+      <Navbar />
+      <div className="state-contents-container">
+        <img className="side-logo-state" src="/assets/nesoj.png" alt="" />
+        <div className="aboutState">
+          <img src="assets/StateLogos/500x500/ASUJ/ASUJ_Colorful.jpg" alt="" />
+          <h3>Honesty is the Best Policy</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+            corrupti, sunt soluta in iste magni nisi cupiditate sapiente beatae
+            laboriosam ipsa iusto molestiae tempore est, sint numquam officia
+            tenetur maxime dolores mollitia. Quos recusandae eligendi est ullam,
+            illo obcaecati corporis ducimus provident doloremque. Sequi libero
+            laboriosam recusandae pariatur, fuga reprehenderit.
+          </p>
+        </div>
+
+        <div className="secondRowAbout container text-center my-3">
+        <h2 className="font-weight-light">Executive Board 2023-34</h2>
+        <div className="row mx-auto my-auto">
+          <Slider {...settings}>
+            {items.map(item => (
+              <div key={item.id} className="px-3">
+                <div className="card card-body cards-aboutUs-Landing">
+                  <Card name={item.name}/>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default State;
