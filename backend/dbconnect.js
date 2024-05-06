99d923dad32733d85d0cbcee60c4a3bbb42f4d48
Mrigankar Sonowal <mrigankarofficial@gmail.com>
2024-05-07 01:23:49 +0530

    Revert dbconnect.js

--- backend/dbconnect.js
+++ backend/dbconnect.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
+const MONGODB_URI = 'mongodb+srv://asuj:Asuj321@asujcluster.uglyh6t.mongodb.net/nesoj';
+// const MONGODB_URI = 'mongodb://127.0.0.1:27017/nesoj';
 
-// It's best to use environment variables for sensitive information
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/nesoj';
+
+
 
 // Connect to MongoDB
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,  // if you use indexes in your schemas, this is recommended
-  useFindAndModify: false // if you use findOneAndUpdate or findOneAndRemove
-}).catch(error => console.error('MongoDB connection error:', error)); // Catch and log any initial connection errors
+});
 
 const db = mongoose.connection;
-
-db.on('error', error => console.error('MongoDB connection error:', error)); // Improved error handling
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => console.log('Connected to MongoDB'));
 
-module.exports = db; // Exporting db is usually more useful for checking the connection status elsewhere
+module.exports = mongoose;
