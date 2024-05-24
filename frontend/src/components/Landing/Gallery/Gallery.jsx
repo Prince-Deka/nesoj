f9139c5b4b1be0c18834eb5b5a689de2b104d859
unknown <princedeka00@gmail.com>
2024-05-24 06:34:05 +0530

    navbar update

--- frontend/src/components/Landing/Gallery/Gallery.jsx
+++ frontend/src/components/Landing/Gallery/Gallery.jsx
@@ -3,19 +3,29 @@ import Navbar from "../NavBar/Navbar";
 import Footer from "../Footer/Footer";
 import Photos from "./Photos/Photos";
 import Videos from "./Videos/Videos";
+import { useLocation } from "react-router-dom";
 
 
 
 function Gallery() {
+  const location = useLocation()
+  const gallery = location.state?.gallery || '';
+  console.log(gallery);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
     <div>
       <Navbar />
-      <Photos />
-      <Videos />
-      <Footer />
+      {gallery === 'photos' && <Photos />}
+      {gallery === 'videos' && <Videos />}
+      {(gallery === 'all' || gallery === '') && (
+        <>
+          <Photos />
+          <Videos />
+        </>
+      )}
     </div>
   );
 }
